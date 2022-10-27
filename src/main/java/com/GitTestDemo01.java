e5921497e4c9a369fccec217fc471d66acc7c384
wulinjie <2461580988@qq.com>
2022-10-27 14:58:38 +0800

    hot-fix first commit

--- src/main/java/com/GitTestDemo01.java
+++ src/main/java/com/GitTestDemo01.java
@@ -3,5 +3,6 @@ package com;
 public class GitTestDemo01 {
     public static void main(String[] args) {
         System.out.println("hello git~~");
+        System.out.println("hello hot-fix git~~");
     }
 }
