8b61b306f2d7698b194afe9f357c0f7a38103c55
wulinjie <2461580988@qq.com>
2022-10-27 16:10:15 +0800

    push first commit

--- src/main/java/com/GitTestDemo01.java
+++ src/main/java/com/GitTestDemo01.java
@@ -2,8 +2,9 @@ package com;
 
 public class GitTestDemo01 {
     public static void main(String[] args) {
-        System.out.println("hello git~~");
-        System.out.println("hello git~~");
-        System.out.println("hello hot-fix git~~");
+        System.out.println("hello git1~~");
+        System.out.println("hello git2~~");
+        System.out.println("hello hot-fix1 git~~");
+        System.out.println("hello hot-fix2 git~~");
     }
 }
